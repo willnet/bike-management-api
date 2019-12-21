f1cc09d6a8973b910f4372556b265d1e7b8f48cf
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2019-12-21 13:09:13 +0100

    showメソッドのリファクタリング

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,22 +1,15 @@
 class BrandsController < ApplicationController
-  def show
-    # http://localhost:3000//brands/1
-    # リクエストされたバイクのブランドに一致する自転車情報を返却する
-    brand = Brand.new(params)
-    brand_info =brand.fetch_brand_info
-    bike = Bike.new(params)
-    bike_info = bike.fetch_bike_info
-    # 2019年1月10日 13時2分加工していく
-    array = []
-    bike_info.each do |bike|
-      json = {
-        id: brand_info[:id],
+  def show # リクエストされたバイクのブランドに一致する自転車情報を返却する
+    brand =Brand.new(params).fetch_brand_info
+    bikes = Bike.new(params).fetch_bike_info
+    response = bikes.map do |bike|
+      {
+        id: brand[:id],
         serial_number: bike[:serial_number],
-        sold_at: bike[:sold_at]
-      }.to_json
-      @res = array.push(json)
+        sold_at: bike[:sold_at]&.strftime("%Y年%m月%d日 %H時%M分") 
+      }
     end
 
-    render json: @res
+    render json: response
   end
 end
